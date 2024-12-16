e36cb0f50b46006d797bef7e6acef7827325b8ea
Ameen <ameen25144@gmail.com>
2024-12-16 17:10:53 +0200

    Additions to Logic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.three_lane_road"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.three_lane_road"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/three_lane_road/MainActivity.kt
+++ app/src/main/java/com/example/three_lane_road/MainActivity.kt
@@ -1,20 +1,85 @@
 package com.example.three_lane_road
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+   private var lives=3
+  private  var currLane=1
+  private  lateinit var rows: List<ViewGroup>
+   private lateinit var matrix: List<List<ImageView>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+            initGame()
+
         }
+
+
+   private fun initGame(){
+        lives=3
+        currLane=1
+        initGrid()
+
+
+
     }
-}
+   private fun initGrid(){
+        rows = listOf(
+           findViewById(R.id.firstRow),
+            findViewById(R.id.secondRow),
+            findViewById(R.id.thirdRow),
+            findViewById(R.id.fourthRow),
+            findViewById(R.id.fifthRow),
+            findViewById(R.id.carRow)
+
+        )
+        matrix=listOf(
+            listOf(
+                findViewById<ImageView>(R.id.stone00),
+                findViewById<ImageView>(R.id.stone01),
+                findViewById<ImageView>(R.id.stone02)),
+            listOf(
+                findViewById<ImageView>(R.id.stone10),
+                findViewById<ImageView>(R.id.stone11),
+                findViewById<ImageView>(R.id.stone12)),
+            listOf(
+            findViewById<ImageView>(R.id.stone20),
+            findViewById<ImageView>(R.id.stone21),
+                findViewById<ImageView>(R.id.stone22)),
+            listOf(
+
+                findViewById<ImageView>(R.id.stone30),
+                findViewById<ImageView>(R.id.stone31),
+                findViewById<ImageView>(R.id.stone32)),
+
+            listOf(
+
+                findViewById<ImageView>(R.id.stone40),
+                findViewById<ImageView>(R.id.stone41),
+                findViewById<ImageView>(R.id.stone42)),
+
+            listOf(
+                findViewById<ImageView>(R.id.carRight),
+                findViewById<ImageView>(R.id.carMid),
+                findViewById<ImageView>(R.id.carLeft))
+        )
+
+       for(i in 0 until matrix.size-1){
+           for(cell in matrix[i])
+               cell.visibility= View.INVISIBLE
+       }
+       matrix[matrix.size-1][0].visibility=View.INVISIBLE
+       matrix[matrix.size-1][currLane].visibility=View.VISIBLE
+       matrix[matrix.size-1][2].visibility=View.INVISIBLE
+    }
+    }
